fa58291c286d095ec7b05e36ab33ae982b3446d7
Fabio Roberto <fabiorvs@globo.com>
2023-08-01 15:53:02 -0300

    Updates

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const dataRoutes = require("./routes/dataRoutes");
-const loggerMiddleware = require('./middleware/loggerMiddleware');
-const swaggerUi = require('swagger-ui-express');
-const specs = require('./swagger');
+const loggerMiddleware = require("./middleware/loggerMiddleware");
+const swaggerUi = require("swagger-ui-express");
+const specs = require("./swagger");
+const cors = require("cors");
 
 // Carregar variáveis de ambiente do arquivo .env
 dotenv.config();
@@ -13,7 +14,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000; // Utilizando variável de ambiente ou a porta 3000 por padrão
 
-
+// Configurar o CORS para todas as rotas
+app.use(cors());
 // Middleware para transformar o corpo da requisição em JSON
 app.use(bodyParser.json());
 
@@ -26,7 +28,7 @@ app.use("/auth", authRoutes);
 // Rotas para manipulação dos dados
 app.use("/data", dataRoutes);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Iniciar o servidor
 app.listen(port, () => {
